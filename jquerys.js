9e2b745f747b3b13a32cfb1f3bc2d7c76611aa45
A. Abdul Mustahab <a.mustahab@gmail.com>
2022-11-01 11:24:19 +0800

    Update jquerys.js

--- jquerys.js
+++ jquerys.js
@@ -1,14 +1,9 @@
-$(function(){
-        $('[datetime]:not(.timeago)').each(function() {
-            var $this = $(this);
-            var data_timeago = $this.attr('datetime');
-            $this.addClass('timeago');
-            if(!$this.attr('title')) {
-                $this.attr('title', data_timeago);
-            }
-            $this.text(get_timeago(data_timeago));
-        });
+function timeago() {
+    $('[datetime]').each(function() {
+        var data_timeago = $(this).attr('datetime');
+        $(this).text(get_timeago(data_timeago));
     });
+
     function get_timeago(t) {
         var e = new Date(t),
             a = 36e5,
@@ -17,8 +12,9 @@ $(function(){
             i = 365 * o,
             n = "yang lalu",
             l = new Date - e;
-        return l &lt; 6e4 ? Math.round(l / 1e3) + " detik " + n : l &lt; a ? Math.round(l / 6e4) + " menit " + n : l &lt; o ? Math.round(l / a) + " jam " + n : l &lt; s ? Math.round(l / o) + " hari " + n : l &lt; i ? Math.round(l / s) + " bulan " + n : Math.round(l / i) + " tahun " + n
+        return l < 6e4 ? Math.round(l / 1e3) + " detik " + n : l < a ? Math.round(l / 6e4) + " menit " + n : l < o ? Math.round(l / a) + " jam " + n : l < s ? Math.round(l / o) + " hari " + n : l < i ? Math.round(l / s) + " bulan " + n : Math.round(l / i) + " tahun " + n
     }
+}
 $(document).ready(function() {
   $('span.close_icon_share, .fullClose').on('click', function() {
     $('.active').removeClass('active');
